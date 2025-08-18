0f75817a64ba250f02c73ab2adca5e9b0a33af18
kawaiTakeru <t-kawai@bfts.co.jp>
2025-08-18 16:05:32 +0900

    Update main.tf

--- terraform/stage2-vnet/main.tf
+++ terraform/stage2-vnet/main.tf
@@ -1,1 +1,29 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.rg_name
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+output "vnet_name" {
+  value = azurerm_virtual_network.this.name
+}
 
