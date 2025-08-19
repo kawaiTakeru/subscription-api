927b81b9a4c215e0fb22ebc05f99653c0b2a7b61
kawaiTakeru <t-kawai@bfts.co.jp>
2025-08-19 13:53:43 +0900

    Update main.tf

--- terraform/stage4b-peering-spoke-to-hub/main.tf
+++ terraform/stage4b-peering-spoke-to-hub/main.tf
@@ -1,13 +1,19 @@
 terraform {
   required_version = ">= 1.4.0"
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.100" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
   }
 }
 
-# このステージは Spoke サブスクリプションで実行
-provider "azurerm" { features {} }
+# spoke サブスクリプションで実行
+provider "azurerm" {
+  features {}
+}
 
+# Hub 側 VNet のリソースIDを組み立て
 locals {
   hub_vnet_id = "/subscriptions/${var.hub_subscription_id}/resourceGroups/${var.hub_rg_name}/providers/Microsoft.Network/virtualNetworks/${var.hub_vnet_name}"
 }
@@ -18,8 +24,7 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   virtual_network_name      = var.spoke_vnet_name
   remote_virtual_network_id = local.hub_vnet_id
 
-  allow_gateway_transit        = false
-  use_remote_gateways          = true   # Hub 側 transit 有効化後に借用
-  allow_virtual_network_access = true
-  allow_forwarded_traffic      = true
+  allow_forwarded_traffic = true
+  allow_gateway_transit   = false
+  use_remote_gateways     = true
 }
