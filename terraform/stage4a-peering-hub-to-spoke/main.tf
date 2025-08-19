1267476d768951d4acb73f8e69b815e4bfd8003c
kawaiTakeru <t-kawai@bfts.co.jp>
2025-08-19 13:14:01 +0900

    Create main.tf

---
+++ terraform/stage4a-peering-hub-to-spoke/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.100" }
+  }
+}
+
+provider "azurerm" { features {} }  # Hub サブスクリプションで実行
+
+locals {
+  spoke_vnet_id = "/subscriptions/${var.spoke_subscription_id}/resourceGroups/${var.spoke_rg_name}/providers/Microsoft.Network/virtualNetworks/${var.spoke_vnet_name}"
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_spoke" {
+  name                      = "hub-to-spoke"
+  resource_group_name       = var.hub_rg_name
+  virtual_network_name      = var.hub_vnet_name
+  remote_virtual_network_id = local.spoke_vnet_id
+
+  allow_gateway_transit       = true
+  use_remote_gateways         = false
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = true
+}
