68443a3453a509e00ac3a3294da5aab883e18b35
kawaiTakeru <t-kawai@bfts.co.jp>
2025-08-29 12:35:02 +0900

    Update variables.tf

--- terraform/stage3-subnet/variables.tf
+++ terraform/stage3-subnet/variables.tf
@@ -1,17 +1,41 @@
-variable "rg_name"      { type = string }
-variable "vnet_name"    { type = string }
-variable "subnet_name"  { type = string }
+variable "rg_name" {
+  type        = string
+  description = "Resource Group name (existing)"
+}
 
-# 追加：Subnet も IPAM から割当
+variable "vnet_name" {
+  type        = string
+  description = "VNet name (existing)"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Subnet name"
+}
+
+# Subnet も IPAM から割当
 variable "ipam_pool_id" {
   description = "Resource ID of IPAM pool to allocate Subnet space from"
   type        = string
 }
+
 variable "subnet_number_of_ips" {
-  description = "How many IPs to allocate to the Subnet (e.g. 256 ≒ /24相当)"
-  type        = string
+  description = "How many IPs to allocate to the Subnet (e.g. 256 ≒ /24)"
+  type        = number
 }
 
-variable "nsg_name"            { type = string }
-variable "vpn_client_pool_cidr"{ type = string }
-variable "allowed_port"        { type = number  default = 3389 }
+variable "nsg_name" {
+  type        = string
+  description = "NSG name"
+}
+
+variable "vpn_client_pool_cidr" {
+  type        = string
+  description = "VPN client pool CIDR for allow-rule"
+}
+
+variable "allowed_port" {
+  type        = number
+  description = "Allowed port from VPN clients (3389 for RDP, 22 for SSH)"
+  default     = 3389
+}
