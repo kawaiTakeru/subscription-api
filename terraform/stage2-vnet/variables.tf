b9faf81601fdd679cff3a1759728061af6e69cc5
kawaiTakeru <t-kawai@bfts.co.jp>
2025-08-29 12:34:03 +0900

    Update variables.tf

--- terraform/stage2-vnet/variables.tf
+++ terraform/stage2-vnet/variables.tf
@@ -2,17 +2,18 @@ variable "rg_name" {
   description = "Resource Group name (existing)"
   type        = string
 }
+
 variable "vnet_name" {
   description = "VNet name"
   type        = string
 }
 
-# 追加：IPAMプールID と VNetに欲しいIP数
 variable "ipam_pool_id" {
   description = "Resource ID of IPAM pool to allocate VNet space from"
   type        = string
 }
+
 variable "vnet_number_of_ips" {
-  description = "How many IPs to allocate to the VNet (e.g. 1024 ≒ /22相当)"
-  type        = string
+  description = "How many IPs to allocate to the VNet (e.g. 1024 ≒ /22)"
+  type        = number
 }
